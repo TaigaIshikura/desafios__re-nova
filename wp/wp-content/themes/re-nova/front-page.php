867c6c92d6a17032161844a6b3ddcb7f3e817ad1
taiga <taiga.ishikura1986@gmail.com>
2026-05-08 09:42:25 +0900

    [

--- wp/wp-content/themes/re-nova/front-page.php
+++ wp/wp-content/themes/re-nova/front-page.php
@@ -38,15 +38,6 @@
                         <div class="swiper-container js-aslider">
                           <div class="swiper-wrapper">
                             <!-- <div class="swiper-slide"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/top/top_photo01.jpg" alt=""></div> -->
-                            <!-- <div class="swiper-slide"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/top/top_photo02.jpg" alt=""></div> -->
-                            <!-- <div class="swiper-slide"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/top/top_photo03.jpg" alt=""></div> -->
-                            <!-- <div class="swiper-slide"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/top/top_photo04.jpg" alt=""></div> -->
-                            <!-- <div class="swiper-slide"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/top/top_photo05.jpg" alt=""></div> -->
-                            <!-- <div class="swiper-slide"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/top/top_photo06.jpg" alt=""></div> -->
-                            <!-- <div class="swiper-slide"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/top/top_photo07.jpg" alt=""></div> -->
-                            <!-- <div class="swiper-slide"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/top/top_photo08.jpg" alt=""></div> -->
-                            <!-- <div class="swiper-slide"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/top/top_photo09.jpg" alt=""></div> -->
-                            <!-- <div class="swiper-slide"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/top/top_photo10.jpg" alt=""></div> -->
                             <div class="swiper-slide"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/top/top_photo11.png" alt="2026年4月 GRAND OPEN！"></div>
                             <div class="swiper-slide"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/top/top_photo12.jpg" alt=""></div>
                             <div class="swiper-slide"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/top/top_photo13.jpg" alt=""></div>
@@ -54,6 +45,15 @@
                             <div class="swiper-slide"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/top/top_photo15.jpg" alt=""></div>
                             <div class="swiper-slide"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/top/top_photo16.jpg" alt=""></div>
                             <div class="swiper-slide"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/top/top_photo17.jpg" alt=""></div>
+                            <div class="swiper-slide"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/top/top_photo02.jpg" alt=""></div>
+                            <div class="swiper-slide"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/top/top_photo03.jpg" alt=""></div>
+                            <div class="swiper-slide"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/top/top_photo04.jpg" alt=""></div>
+                            <div class="swiper-slide"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/top/top_photo05.jpg" alt=""></div>
+                            <div class="swiper-slide"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/top/top_photo06.jpg" alt=""></div>
+                            <div class="swiper-slide"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/top/top_photo07.jpg" alt=""></div>
+                            <div class="swiper-slide"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/top/top_photo08.jpg" alt=""></div>
+                            <div class="swiper-slide"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/top/top_photo09.jpg" alt=""></div>
+                            <div class="swiper-slide"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/top/top_photo10.jpg" alt=""></div>
                           </div>
                         </div>
                         <div class="m-slider-1__next js-asliderNext"></div>
